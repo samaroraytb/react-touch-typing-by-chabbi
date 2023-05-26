5d92aa0339a5f511993fa54ad0472b510b6aa0d9
shubham <samaroraytb@gmail.com>
2023-05-27 01:04:15 +0530

    Custom Input Added

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import Header from "../Header/Header";
 import "./body.css";
@@ -6,13 +6,30 @@ import "./body.css";
 function Body() {
   const selectorObject = useSelector((reducer) => reducer.textData);
 
+  const [userTyped, updateTyped] = useState("")
+  const [score, updateScore] = useState({ matchScore: 0, nonMatched: 0 })
+
+  useEffect(() => {
+    const isMatched = userTyped.charAt(userTyped.length - 1) === selectorObject[userTyped.length - 1]
+    if(isMatched === true){
+      updateScore({...score, matchScore: score.matchScore + 1})
+    }else{
+      updateScore({...score, nonMatched: score.nonMatched + 1})
+    }
+  }, [userTyped])
+
+  const userTestStart = event => {
+    updateTyped(event.target.value)
+  }
+  console.log(score)
   return (
     <>
       <Header />
       <div className="body-main-container">
         <div className="inside-container-body">
           <p className="demo-data-body">{selectorObject}</p>
-          <textarea className="body-user-input"></textarea>
+          <textarea onChange={userTestStart} className="body-user-input" value={userTyped}></textarea>
+          <p className="home-message-block">Doing Well Buddy!!</p>
         </div>
       </div>
     </>
--- src/components/customText/CustomText.jsx
+++ src/components/customText/CustomText.jsx
@@ -1,7 +1,7 @@
 import Header from '../Header/Header'
 import './customtext.css'
 import { useDispatch } from 'react-redux'
-import {setData} from '../../redux/actions/index'
+import { setData } from '../../redux/actions/index'
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
@@ -9,26 +9,41 @@ const CustomText = () => {
     const dispachFn = useDispatch();
     const navigate = useNavigate();
     const [userText, changeUserText] = useState('')
+    const [errorMsg, updateMsg] = useState(false)
 
     const changedUserInput = event => {
+        updateMsg(false)
         changeUserText(event.target.value)
     }
 
-    const userTextSubmittion = () => {
-        dispachFn(setData())
-        navigate('/', {replace: true});
+    const goBackFromUser = () => {
+        navigate('/', { replace: true });
     }
 
-    return(
+    const userTextSubmittion = () => {
+        if (userText !== "") {
+            updateMsg(false)
+            dispachFn(setData(userText))
+            navigate('/', { replace: true });
+        }else{
+            updateMsg(true)
+        }
+    }
+
+    return (
         <>
-        <Header />
-        <div className='custom-text-main-bg'>
-            <div className='custom-text-innner-bg'>
-                <h1 className='custom-text-main-heading'>Provide Custom Text for Touch Typing</h1>
-                <textarea onChange={changedUserInput} className='user-input-text-area' value={userText}></textarea>
-                <button className='custom-text-button-style' type="button" onClick={userTextSubmittion}>Submit Text</button>
+            <Header />
+            <div className='custom-text-main-bg'>
+                <div className='custom-text-innner-bg'>
+                    <h1 className='custom-text-main-heading'>Provide Custom Text for Touch Typing</h1>
+                    <textarea onChange={changedUserInput} className='user-input-text-area' value={userText}></textarea>
+                    <div>
+                    <button className='custom-text-button-style' type="button" onClick={userTextSubmittion}>Submit Text</button>
+                    <button className='custom-text-go-back-button' type="button" onClick={goBackFromUser}>Go back</button>
+                    </div>
+                    {errorMsg && <p className='error-message-custom-text'>*please provide some text data</p>}
+                </div>
             </div>
-        </div>
         </>
     )
 }
