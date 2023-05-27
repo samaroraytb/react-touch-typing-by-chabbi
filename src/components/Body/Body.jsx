358770aca4cbd13b2f112959aa719fc1428daaf8
shubham <samaroraytb@gmail.com>
2023-05-27 13:03:16 +0530

    Fun Added

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,35 +1,87 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import { useSelector } from "react-redux";
 import Header from "../Header/Header";
+import { MdRunningWithErrors } from "react-icons/md";
+import { FaTasks } from "react-icons/fa";
+import { AiFillCheckCircle } from "react-icons/ai";
 import "./body.css";
 
 function Body() {
   const selectorObject = useSelector((reducer) => reducer.textData);
 
-  const [userTyped, updateTyped] = useState("")
-  const [score, updateScore] = useState({ matchScore: 0, nonMatched: 0 })
+  const [userTyped, updateTyped] = useState("");
+  const [score, updateScore] = useState({ matchScore: 0, nonMatched: 0 });
+  const [accuracy, updateAccuracy] = useState(0)
 
-  useEffect(() => {
-    const isMatched = userTyped.charAt(userTyped.length - 1) === selectorObject[userTyped.length - 1]
-    if(isMatched === true){
-      updateScore({...score, matchScore: score.matchScore + 1})
-    }else{
-      updateScore({...score, nonMatched: score.nonMatched + 1})
+  const keyPressedUser = (event) => {
+    const isMatched = event.key === selectorObject[userTyped.length];
+
+    if (isMatched === true) {
+      updateScore({ ...score, matchScore: score.matchScore + 1 });
+    } else {
+      updateScore({ ...score, nonMatched: score.nonMatched + 1 });
     }
-  }, [userTyped])
+  };
 
-  const userTestStart = event => {
-    updateTyped(event.target.value)
+  const userTestStart = (event) => {
+    updateTyped(event.target.value);
+  };
+
+  const calculateProgress = () => {
+    let progress = 0;
+
+    if (userTyped.length >= selectorObject.length) {
+      progress = 100;
+    } else {
+      progress = (userTyped.length / selectorObject.length) * 100;
+    }
+
+    return progress.toFixed(2);
+  };
+
+  const calculateTheAccuracy = () => {
+    const validScore = score.matchScore - score.nonMatched;
+    const calucatePercentage = (validScore/selectorObject.length)*100;
+    const toFixedTwo = calucatePercentage.toFixed(2);
+    updateAccuracy(toFixedTwo);
   }
-  console.log(score)
+  
   return (
     <>
       <Header />
       <div className="body-main-container">
         <div className="inside-container-body">
           <p className="demo-data-body">{selectorObject}</p>
-          <textarea onChange={userTestStart} className="body-user-input" value={userTyped}></textarea>
-          <p className="home-message-block">Doing Well Buddy!!</p>
+          <textarea
+            id="#userTypedTextID"
+            onKeyPress={keyPressedUser}
+            onChange={userTestStart}
+            className="body-user-input"
+            value={userTyped}
+          ></textarea>
+          <div className="complete-body-res-container">
+            <div className="body-result-icon-text-container">
+              <MdRunningWithErrors className="body-error-icon" size={25} />
+              <p className="body-res-text-style">
+                Mistakes: <span>{score.nonMatched}</span>
+              </p>
+            </div>
+            <div className="body-result-icon-text-container">
+              <FaTasks className="body-task-icon" size={25} />
+              <p className="body-res-text-style">
+                Progress: <span>{calculateProgress()}%</span>
+              </p>
+            </div>
+          </div>
+          <div className="calculate-accuracy-button-container">
+            <button onClick={calculateTheAccuracy} type="button" className="calculate-accuracy-button">Calculate Accuracy</button>
+            <div className="body-res-calculate-accuracy-content-container">
+              <AiFillCheckCircle className="body-check-icon" size={25} />
+              <p className="body-res-text-style">
+                Accuracy: <span>{accuracy}%</span>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,24 @@
 import "./header.css";
 import { IoIosMail } from "react-icons/io";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import { BiReset } from "react-icons/bi";
+import { useDispatch } from "react-redux";
+import { resetData } from "../../redux/actions/index";
+
 
 const Header = () => {
   const navigate = useNavigate();
+  const dispachFn = useDispatch();
 
   const clickedOnCustomText = () => {
-    navigate('/custom-text', {replace: true});
-  }
+    navigate("/custom-text", { replace: true });
+  };
+
+  const clickedOnReset = () => {
+    dispachFn(resetData());
+  };
+
+  const email = "samaroraytb@gmail.com";
 
   return (
     <div className="main-header-container">
@@ -15,10 +26,19 @@ const Header = () => {
         Touch Typing <span>by Shubham</span>
       </div>
       <div className="header-user-input-email">
-        <button className="header-main-input-button" type="button" onClick={clickedOnCustomText}>
+        <button
+          className="header-main-input-button"
+          type="button"
+          onClick={clickedOnCustomText}
+        >
           Submit your text
         </button>
-        <IoIosMail className="header-mail-icon" />
+        <div className="header-icon-container">
+        <BiReset size={30} onClick={clickedOnReset} />
+        <a  href={`mailto:${email}`} className="header-mail-icon" style={{color: "white"}}>
+          <IoIosMail className="header-mail-icon"/>
+        </a>
+        </div>
       </div>
     </div>
   );
